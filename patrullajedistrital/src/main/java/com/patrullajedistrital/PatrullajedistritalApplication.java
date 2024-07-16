10feecf0cf706c53688953f0258678e32760a617
PedroManuelJM <mpedrojurado@gmail.com>
2024-07-16 14:47:49 -0500

    backend

---
+++ patrullajedistrital/src/main/java/com/patrullajedistrital/PatrullajedistritalApplication.java
@@ -0,0 +1,15 @@
+package com.patrullajedistrital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PatrullajedistritalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PatrullajedistritalApplication.class, args);
+	}
+
+}
+// cd AxonServer/AxonServer-2024.0.4
+//java -jar axonserver.jar
